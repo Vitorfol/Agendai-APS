fcee8a11f44339575d7585f4eea840c859adb8fd
Vitor Fontenele <142616878+Vitorfol@users.noreply.github.com>
2025-11-21 18:46:29 -0300

    jwt config

---
+++ backend/src/core/config.py
@@ -0,0 +1,14 @@
+from pydantic_settings import BaseSettings
+
+class Settings(BaseSettings):
+    API_V1_STR: str = "/api"
+    PROJECT_NAME: str = "Agendai"
+    ALGORITHM: str = "HS256"
+    SECRET_KEY: str 
+    ACCESS_TOKEN_EXPIRE_MINUTES: int 
+    REFRESH_TOKEN_EXPIRE_DAYS: int 
+    
+    class Config:
+        env_file = ".env"
+        
+settings = Settings()
