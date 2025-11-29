14b76db3aa54dd335f034d59a10ac834eb0d77c3
iandomingos777 <ianbrunodomingos@gmail.com>
2025-11-29 19:03:04 -0300

    Revert "criação de get dos cursos"
    This reverts commit ccdfde1e8df0eafe67d95daf83ac3a1f13e265c2.

--- backend/src/database/popule.py
+++ backend/src/database/popule.py
@@ -55,18 +55,7 @@ def popular_banco():
         nome="Ciência da Computação",
         sigla="BCC",
         email="bcc@uft.edu.br",
-        id_universidade=univ.id,         # Mudança: idUniversidade -> id_universidade
-        graduacao=True                   # Novo campo adicionado
-    )
-    session.add(curso)
-    session.flush()
-
-    curso = Curso(
-        nome="Matemática",
-        sigla="MAT",
-        email="bmat@uft.edu.br",
-        id_universidade=univ.id,        # Mudança: idUniversidade -> id_universidade
-        graduacao=False                  # Novo campo adicionado
+        id_universidade=univ.id         # Mudança: idUniversidade -> id_universidade
     )
     session.add(curso)
     session.flush()
