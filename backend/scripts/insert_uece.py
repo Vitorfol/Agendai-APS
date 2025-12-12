abecaa5ad04c494aec856173e8ff7257647513f9
ThisIsmael <ismaelsousafcastro@gmail.com>
2025-12-11 21:38:32 -0300

    ajeitei o script

--- backend/scripts/insert_uece.py
+++ backend/scripts/insert_uece.py
@@ -11,7 +11,7 @@ import os
 from datetime import datetime
 
 current_dir = os.path.dirname(os.path.abspath(__file__))
-project_root = os.path.abspath(os.path.join(current_dir, '..', '..'))
+project_root = os.path.abspath(os.path.join(current_dir, '..'))
 sys.path.append(project_root)
 
 from src.models.models import Universidade
